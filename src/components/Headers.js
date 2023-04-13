34059965bff464add0f74c260b4ecdb6ca4c16f2
HemantWD <hemantajaysharma@gmail.com>
2023-04-13 23:04:33 +0530

    navigation

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -10,7 +10,7 @@ const Headers = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="/general">
                 Home
               </a>
             </li>
